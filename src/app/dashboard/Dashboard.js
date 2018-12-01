ab9a64c548a1633be271f921ff8eb5fa734491d5
Squishy123 <christian.gnaw@gmail.com>
2018-11-30 21:49:20 -0500

    Reverted router links to a links and added select cluster view

--- src/app/dashboard/Dashboard.js
+++ src/app/dashboard/Dashboard.js
@@ -20,10 +20,12 @@ export default class Dashboard extends React.Component {
                 state = "select-node";
             }
         }
+        console.log(state);
         this.state = { dashboardState: state, login: false, groupCards: [] };
 
         this.expiryAccess = this.expiryAccess.bind(this);
         this.getGroups = this.getGroups.bind(this);
+        this.getClusters = this.getClusters.bind(this);
     }
 
     async expiryAccess() {
@@ -48,22 +50,69 @@ export default class Dashboard extends React.Component {
                     'access_token': this.state.access_token
                 },
             }).then(res => res.json());
-            groups.forEach(e => {
-                let card = <Groupcard name={e.name} description={e.description} group_id={e._id} />;
-                this.setState({
-                    groupCards: this.state.groupCards.concat([card])
+            if (groups.length > 0) {
+                groups.forEach(e => {
+                    let card = <Groupcard name={e.name} description={e.description} group_id={e._id} />;
+                    this.setState({
+                        groupCards: this.state.groupCards.concat([card])
+                    });
                 });
-            });
+            } else {
+                this.setState({
+                    groupCards: this.state.groupCards.concat([<h1>No Clusters Here...</h1>])
+                });
+            }
         } catch (err) {
             console.log(err);
         }
     }
 
+    async getClusters() {
+        try {
+            let clusters = await fetch(`${process.env.REACT_APP_SERVER_HOST}/api/clusters`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'access_token': this.state.access_token,
+                    'group_id': this.props.match.params.group_id
+                }
+            }).then(res => res.json());
+            if (clusters.length > 0) {
+                clusters.forEach(e => {
+                    let card = <Groupcard name={e.name} description={e.description} group_id={`${this.props.match.params.group_id}/${e._id}`} />;
+                    this.setState({
+                        groupCards: this.state.groupCards.concat([card])
+                    });
+                });
+            } else {
+                this.setState({
+                    groupCards: this.state.groupCards.concat([<h1>No Clusters Here...</h1>])
+                });
+            }
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async checkState() {
+        let state = "select-group";
+        //determine which of the 3 states we are in
+        if (this.props.match.params.group_id) {
+            state = "select-cluster";
+            if (this.props.match.params.cluster_id) {
+                state = "select-node";
+            }
+        }
+        this.setState({dashboardState: state})
+    }
 
     async componentDidMount() {
         let expiry = await this.expiryAccess();
         if (!expiry && this.state.dashboardState === 'select-group') {
             this.getGroups();
+        } else if (!expiry && this.state.dashboardState === 'select-cluster') {
+            this.getClusters();
         }
     }
 
--- src/app/dashboard/groupcard/Groupcard.js
+++ src/app/dashboard/groupcard/Groupcard.js
@@ -14,7 +14,7 @@ export default class Groupcard extends React.Component {
             <div className="group-card">
                 <h2 className="card-title">{this.props.name}</h2>
                 <p className="card-description">{this.props.description}</p>
-                <Link className="card-link button" to={`/dashboard/${this.props.group_id}`}>Clusters</Link>
+                <a className="card-link button" href={`/dashboard/${this.props.group_id}`} >Go To</a>
             </div>
         )
     }
